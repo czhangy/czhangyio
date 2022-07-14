14a4940126ac5adf0ae016a483dabe8a9b23049b
Charles Zhang <charleszhangmb@gmail.com>
2022-07-14 12:24:34 -0700

    ribbons done

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -4,6 +4,9 @@ import styles from "./ProjectCard.module.scss";
 import Project from "@/models/Project";
 // Helper function
 import { toSlug } from "@/utils/helpers";
+// Next
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
     project: Project;
@@ -14,12 +17,40 @@ const ProjectCard: React.FC<Props> = ({ project }: Props) => {
         <div
             className={styles["project-card"]}
             style={{
-                background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/assets/projects/${toSlug(
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/assets/projects/${toSlug(
                     project.name
-                )}/thumbnail.webp');`,
+                )}/thumbnail.webp')`,
             }}
         >
-            {project.name}
+            <div className={`${styles.ribbon} ${styles.top}`}>
+                {project.name}
+            </div>
+            <div className={`${styles.ribbon} ${styles.bottom}`}>
+                {project.gitLink ? (
+                    <Link href={project.gitLink} target="_blank">
+                        <a className={styles["link-icon"]}>
+                            <Image
+                                src="/assets/icons/github.svg"
+                                alt="GitHub link"
+                                layout="fill"
+                                objectFit="contain"
+                            />
+                        </a>
+                    </Link>
+                ) : (
+                    ""
+                )}
+                <Link href={project.link} target="_blank">
+                    <a className={styles["link-icon"]}>
+                        <Image
+                            src="/assets/icons/link.svg"
+                            alt="Site link"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </a>
+                </Link>
+            </div>
         </div>
     );
 };
